157793cbbd91e9dc7be0b783aa309fc735df85bc
Jens <jensonguffick@yahoo.com>
2026-03-27 12:02:14 +0000

    Clear branch down

--- maths/content.js
+++
@@ -1,9 +0,0 @@
-/*
-    ----------------------------------------------------
-    -  Sparx Maths has not currently been made and is  -
-    -   not functional. Stay tuned for later updates   -
-    ----------------------------------------------------
-*/
-
-console.warn("Sparx Maths AI is not functional  ")
-console.warn("Check for updates in the meanwhile")
